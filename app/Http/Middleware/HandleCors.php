c35dda55dc446e030f7bc12db2bb9f8b7a3073eb
liftoff24 <minol24112000@gmail.com>
2024-08-10 23:00:11 +0300

    Add backend code

---
+++ app/Http/Middleware/HandleCors.php
@@ -0,0 +1,21 @@
+namespace App\Http\Middleware;
+
+use Closure;
+
+class HandleCors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+                ->header('Access-Control-Allow-Origin', 'https://product-tracker.onrender.com')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+    }
+}
--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,12 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    
+    'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://product-tracker.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
@@ -30,5 +31,6 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,
+
 
 ];
